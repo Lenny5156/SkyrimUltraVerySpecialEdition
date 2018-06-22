87ba77eea951fa54bca779e81a66d47a349cc270
MaPf06 <pfeutimarcel@gmail.com>
2018-06-22 13:16:41 +0100

    Added Name decision

--- SkyrimUltraVerySpecialEdition/src/functions/RaceMenu.java
+++ SkyrimUltraVerySpecialEdition/src/functions/RaceMenu.java
@@ -5,9 +5,15 @@ import data.Pictures;
 
 public class RaceMenu extends Basic{
 	
-	private String[] character = new String[2];
+	private String[] character = new String[3];
 	private String raceOptions = "Choose your race \n 1.HighElve, 2.Argonian, 3.WoodElve, 4.Breton, 5.DarkElve, 6.Imperial, 7.Khajiit, 8.Nord, 9.Orc, 10.Redguard \n Write the first letter of the Race to choose it.";
 	
+	/* Creates a dialog in which the player can choose his gender, race and name
+	 * The informations about the player are returned in a String[]
+	 * 0 = gender
+	 * 1 = Race
+	 * 2 = Name
+	 */
 	public String[] createCharacter() {
 		clear();
 		output("Are you a man(m) or a Woman(w)?");
@@ -25,7 +31,10 @@ public class RaceMenu extends Basic{
 			showRace(ii, character[0]);
 			if (ii < 11 && ii > 0) { 
 				output("Are you sure? (y/n)");
-				if  (input.next().equals("y") ) { 
+				if  (input.next().equals("y") ) {
+					clear();
+					output("What is your name prisoner?");
+					character[2] = input.next();
 					return character;
 				}
 			}
@@ -33,7 +42,7 @@ public class RaceMenu extends Basic{
 		
 	}
 	
-	
+	//Just shows a "picture" of the Race and sets the Race in the character[]
 	private void showRace(int raceID, String gender) {
 		Pictures p = new Pictures();
 		switch (raceID) {
