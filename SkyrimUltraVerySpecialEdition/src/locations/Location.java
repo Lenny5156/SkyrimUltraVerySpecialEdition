f1827de751e28e61257423162e087291c466c91b
MaPf06 <pfeutimarcel@gmail.com>
2018-06-22 14:30:07 +0100

    Improved map and player classes

--- SkyrimUltraVerySpecialEdition/src/locations/Location.java
+++ SkyrimUltraVerySpecialEdition/src/locations/Location.java
@@ -2,4 +2,7 @@ package locations;
 
 public abstract class Location {
 	public static String locationName;
+	public static String area;
+	public static String map;
+	public static boolean discovered = false;
 }
